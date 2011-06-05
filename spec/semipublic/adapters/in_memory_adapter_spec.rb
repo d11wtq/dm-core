24f6771b99f4b336ecde7ad069704314b171227e
Chris Corbyn <chris@w3style.co.uk>
2011-06-05 18:55:15 +1000

    Write spec to demonstrate in-memory adapter doesn't correctly reload many-to-many associations

--- spec/semipublic/adapters/in_memory_adapter_spec.rb
+++ spec/semipublic/adapters/in_memory_adapter_spec.rb
@@ -7,6 +7,41 @@ describe 'Adapter' do
 
       it_should_behave_like 'An Adapter'
 
+      describe 'reloading complex associations' do
+        before :all do
+          class ::Post
+            include DataMapper::Resource
+
+            property :id, Serial
+            has n, :tags, :through => Resource
+          end
+
+          class Tag
+            include DataMapper::Resource
+
+            property :id, Serial
+            has n, :posts, :through => Resource
+          end
+        end
+
+        let(:post) do
+          object = Post.new
+          object.tags << Tag.new
+          object.tags << Tag.new
+          object.tags << Tag.new
+          object.save
+          object
+        end
+
+        it 'should be able to get all associations' do
+          post.should have(3).tags
+        end
+
+        it 'should still be able to get all associations after #reload' do
+          post.reload.should have(3).tags
+        end
+      end
+
     end
   end
 end
